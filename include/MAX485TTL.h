28db554851665050f8f9c21bb74cd3f88173a862
Rik <rik.vos01@gmail.com>
2023-09-13 15:55:36 +0200

    [Add] Print to stream

--- include/MAX485TTL.h
+++ include/MAX485TTL.h
@@ -3,50 +3,57 @@
 
 #include <Arduino.h>
 
-/// @brief Stream must be opened before passing to this object (Serial.begin(Baudrate)). Note that the SofwareSerial maximum baudrate is less then the HardwareSerial.
-class RS485 : public Stream
+/// @brief
+class RS485 : private Stream
 {
-private:
-    uint8_t mode_output;
-    uint8_t de_pin;
-    uint8_t re_pin;
-    uint16_t buffersize;
-    uint16_t timeout_millis;
-    Stream *serial;
-    char *buffer;
-
 public:
+    // using Print::print;
+    // using Print::println;
+    // using Stream::readString;
+
     /// @brief Constructor using only the necessary
     /// @param de_pin Driver output enable pin number
     /// @param re_pin Receiver output enable pin number
-    /// @param serial Stream to which the module is connected
-    RS485(uint8_t de_pin, uint8_t re_pin, Stream *serial);
+    RS485(uint8_t de_pin, uint8_t re_pin);
 
     /// @brief Constructor with buffersize setting
     /// @param de_pin Driver output enable pin number
     /// @param re_pin Receiver output enable pin number
-    /// @param serial Stream to which the module is connected
     /// @param buffersize Size of wich the buffer must be
-    RS485(uint8_t de_pin, uint8_t re_pin, Stream *serial, uint16_t buffersize);
+    RS485(uint8_t de_pin, uint8_t re_pin, uint16_t buffersize);
 
     /// @brief Constructor with timeout setting
     /// @param de_pin Driver output enable pin number
     /// @param re_pin Receiver output enable pin number
-    /// @param serial Stream to which the module is connected
     /// @param buffersize Size of wich the buffer must be
     /// @param timeout_millis Timeout of input stream for a message
-    RS485(uint8_t de_pin, uint8_t re_pin, Stream *serial, uint16_t buffersize, uint16_t timeout_millis);
+    RS485(uint8_t de_pin, uint8_t re_pin, uint16_t buffersize, uint16_t timeout_millis);
+
+    /**
+     * @brief Destroy the RS485 object
+     * Delete buffer and remove pointers
+     */
+    ~RS485(void);
+
+    /**
+     * @brief Set the Stream to the wanted serial stream.
+     * Stream must be opened before passing to this object (Serial.begin(Baudrate)).
+     * Note that the SofwareSerial maximum baudrate is less then the HardwareSerial.
+     *
+     * @param serial memory address of serial stream
+     */
+    void set_serial(Stream *serial);
 
     /// @brief Function used to toggle the DE and RE pin to let the module accept incomming data
     /// @param mode INPUT(0) or OUTPUT(1)
     void set_mode(uint8_t);
 
     /// @brief Function used to get the number of bytes available in de input buffer which holds 64 bytes
-    /// @return Number of bytes available
+    /// @return Number of bytes available, if stream not available -1
     int available(void);
 
     /// @brief Function used to look at the first byte of the input buffer without taking it out
-    /// @return First character of the buffer
+    /// @return First character of the buffer, if stream not available -1
     int peek(void);
 
     /// @brief Function used to read the first byte of the incomming data
@@ -54,7 +61,7 @@ public:
     int read(void);
 
     /// @brief Function used to get the number of bytes available for writing in the serial buffer before blocking
-    /// @return Number of bytes (characters)
+    /// @return Number of bytes (characters), if stream not available -1
     int availableForWrite(void);
 
     /// @brief Function that is a blocking call to wait for all serial data to be sent
@@ -62,14 +69,28 @@ public:
 
     /// @brief Function to send a single byte or multiple
     /// @param data is the byte that will be sent
-    /// @return Amount of bytes written
+    /// @return Amount of bytes written, if stream not available -1
     size_t write(uint8_t);
-    using Print::print;
-    using Print::write;
+
+    size_t print(String);
 
     /// @brief Function used to read input data
-    /// @return char* of the input data
-    char *receive_rs485(void);
+    void receive(void);
+
+    bool isNewData(void);
+
+    String getNewData(void);
+
+private:
+    bool newData;
+
+    uint8_t mode_output;
+    uint8_t de_pin;
+    uint8_t re_pin;
+    uint16_t buffersize;
+    uint16_t timeout_millis;
+    Stream *serial;
+    char *buffer;
 };
 
 #endif
--- src/MAX485TTL.cpp
+++ src/MAX485TTL.cpp
@@ -1,39 +1,25 @@
 #include <Arduino.h>
-#include <MAX485TTL.h>
+#include <max485ttl.h>
 
-/// @brief Constructor using only the necessary
-/// @param de_pin Driver output enable pin number
-/// @param re_pin Receiver output enable pin number
-/// @param serial Stream to which the module is connected
-RS485::RS485(uint8_t de_pin, uint8_t re_pin, Stream *serial)
+RS485::RS485(uint8_t de_pin, uint8_t re_pin)
 {
-    RS485(de_pin, re_pin, serial, 64);
+    RS485(de_pin, re_pin, 64);
 };
 
-/// @brief Constructor with buffersize setting
-/// @param de_pin Driver output enable pin number
-/// @param re_pin Receiver output enable pin number
-/// @param serial Stream to which the module is connected
-/// @param buffersize Size of wich the buffer must be
-RS485::RS485(uint8_t de_pin, uint8_t re_pin, Stream *serial, uint16_t buffersize)
+RS485::RS485(uint8_t de_pin, uint8_t re_pin, uint16_t buffersize)
 {
-    RS485(de_pin, re_pin, serial, buffersize, 1000);
+    RS485(de_pin, re_pin, buffersize, 1000);
 };
 
-/// @brief Constructor with timeout setting
-/// @param de_pin Driver output enable pin number
-/// @param re_pin Receiver output enable pin number
-/// @param serial Stream to which the module is connected
-/// @param buffersize Size of wich the buffer must be
-/// @param timeout_millis Timeout of input stream for a message
-RS485::RS485(uint8_t de_pin, uint8_t re_pin, Stream *serial, uint16_t buffersize, uint16_t timeout_millis)
+RS485::RS485(uint8_t de_pin, uint8_t re_pin, uint16_t buffersize, uint16_t timeout_millis)
 {
     this->de_pin = de_pin;
     this->re_pin = re_pin;
-    this->serial = serial;
+    this->serial = 0;
     this->buffersize = buffersize;
     this->buffer = new char[buffersize];
     this->timeout_millis = timeout_millis;
+    this->newData = false;
 
     // Set pins to output
     pinMode(de_pin, OUTPUT);
@@ -43,8 +29,17 @@ RS485::RS485(uint8_t de_pin, uint8_t re_pin, Stream *serial, uint16_t buffersize
     set_mode(INPUT);
 };
 
-/// @brief Function used to toggle the DE and RE pin to let the module accept incomming data
-/// @param mode INPUT(0) or OUTPUT(1)
+RS485::~RS485()
+{
+    delete this->buffer;
+    this->serial = nullptr;
+};
+
+void RS485::set_serial(Stream *serial)
+{
+    this->serial = serial;
+}
+
 void RS485::set_mode(uint8_t mode)
 {
     if (mode_output == mode)
@@ -52,87 +47,170 @@ void RS485::set_mode(uint8_t mode)
         return;
     }
 
-    // Set the RS485 driver pin values
-    digitalWrite(de_pin, mode);
-    digitalWrite(re_pin, mode);
-    mode_output = mode;
+    if (mode == INPUT)
+    {
+        digitalWrite(de_pin, LOW);
+        digitalWrite(re_pin, LOW);
+        mode_output = mode;
+    }
+    else if (mode == OUTPUT)
+    {
+        digitalWrite(de_pin, HIGH);
+        digitalWrite(re_pin, HIGH);
+        mode_output = mode;
+    }
 }
 
-/// @brief Function used to get the number of bytes available in de input buffer which holds 64 bytes
-/// @return Number of bytes available
 int RS485::available(void)
 {
     set_mode(INPUT);
-    return serial->available();
+    if (serial)
+    {
+        return serial->available();
+    }
+
+    return -1;
 }
 
-/// @brief Function used to look at the first byte of the input buffer without taking it out
-/// @return first character of the buffer
 int RS485::peek(void)
 {
-    return serial->peek();
+    if (serial)
+    {
+        return serial->peek();
+    }
+
+    return -1;
 }
 
-/// @brief Function used to read the first byte of the incomming data
-/// @return first byte or -1 if not available
 int RS485::read(void)
 {
     set_mode(INPUT);
-    return serial->read();
+    if (serial)
+    {
+        return serial->read();
+    }
+
+    return -1;
 }
 
-/// @brief Function used to get the number of bytes available for writing in the serial buffer before blocking
-/// @return Number of bytes (characters)
 int RS485::availableForWrite(void)
 {
-    return serial->availableForWrite();
+    if (serial)
+    {
+        return serial->availableForWrite();
+    }
+
+    return -1;
 }
 
-/// @brief Function that is a blocking call to wait for all serial data to be sent
 void RS485::flush(void)
 {
     set_mode(OUTPUT);
-    serial->flush();
+    if (serial)
+    {
+        return serial->flush();
+    }
 }
 
-/// @brief Function to send a single byte or multiple
-/// @param data is the byte that will be sent
-/// @return Amount of bytes written
 size_t RS485::write(uint8_t data)
 {
     set_mode(OUTPUT);
-    return serial->write(data);
-}
-
-/// @brief Function used to read input data
-/// @return char* of the input data
-char *RS485::receive_rs485(void)
-{
-    set_mode(INPUT);
-
-    // Wait for a message to be received
-    unsigned long start = millis();
-    while (!serial->available())
+    if (serial)
     {
-        if (millis() - start > timeout_millis)
-            return "";
+        return serial->write(data);
     }
 
-    if (serial->available())
+    return -1;
+}
+
+size_t RS485::print(String data)
+{
+    set_mode(OUTPUT);
+    if (serial)
     {
-        int length = serial->readBytes(buffer, buffersize);
-        if (length > 0)
-        {
-            // Coppy the data out of the buffer
-            char *data = new char[length + 1];
-            memcpy(data, buffer, length);
+        return serial->print(data);
+    }
 
-            // Add null termination to make it a string
-            data[length] = '\0';
+    return -1;
+}
 
-            return data;
-        }
+bool RS485::isNewData(void)
+{
+    return newData;
+}
+
+String RS485::getNewData(void)
+{
+    if (newData)
+    {
+        newData = false;
+        return String(buffer);
     }
 
     return "";
 }
+
+void RS485::receive(void)
+{
+    static byte index = 0;
+    char endMarker = '\n';
+    char newChar;
+
+    while (Serial.available() > 0 && newData == false)
+    {
+        newChar = Serial.read();
+
+        if (newChar != endMarker)
+        {
+            buffer[index] = newChar;
+            index++;
+            if (index >= buffersize)
+            {
+                index = buffersize - 1;
+            }
+        }
+        else
+        {
+            buffer[index] = '\0'; // terminate the string
+            index = 0;
+            newData = true;
+        }
+    }
+}
+
+// String RS485::receive_rs485(void)
+// {
+//     set_mode(INPUT);
+
+//     if (!serial)
+//     {
+//         return "";
+//     }
+
+//     // Wait for a message to be received
+//     unsigned long start = millis();
+//     while (!serial->available())
+//     {
+//         if (millis() - start > timeout_millis)
+//             return new char[0];
+//     }
+
+//     if (serial->available())
+//     {
+//         int length = serial->readBytes(buffer, buffersize);
+//         if (length > 0)
+//         {
+//             // Coppy the data out of the buffer
+//             char *data = new char[length + 1];
+//             memcpy(data, buffer, length);
+
+//             // Add null termination to make it a string
+//             data[length] = '\0';
+//             String stringData(data);
+//             delete[] data;
+//             return stringData;
+//         }
+//     }
+
+//     return "";
+// }
